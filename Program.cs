5c536f2f51eb3d92d89bf196f43422c434bfc3c1
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-09-04 12:30:35 +0300

    Added testContactInfo On PresentationLayer Project

--- Program.cs
+++ Program.cs
@@ -6,9 +6,31 @@ namespace ContactsConsoleAppPresentationLayer
 {
     internal class Program
     {
+        public static void testFindContactByID(int ID)
+        {
+            clsContact ContactInfo = clsContact.Find(ID);
+            if (ContactInfo != null)
+            {
+                Console.WriteLine("----Contact Found-----");
+                Console.WriteLine(ContactInfo.ID);
+                Console.WriteLine(ContactInfo.FirstName);
+                Console.WriteLine(ContactInfo.LastName);
+                Console.WriteLine(ContactInfo.Email);
+                Console.WriteLine(ContactInfo.Phone);
+                Console.WriteLine(ContactInfo.Address);
+                Console.WriteLine(ContactInfo.DateOfBirth);
+                Console.WriteLine(ContactInfo.ImagePath);
+                Console.WriteLine(ContactInfo.CountryID);
+            }
+
+            else
+            {
+                Console.WriteLine("Contact Not Found:-(");
+            }
+        }
         static void Main(string[] args)
         {
-            
+            testFindContactByID(2);
         }
     }
 }
