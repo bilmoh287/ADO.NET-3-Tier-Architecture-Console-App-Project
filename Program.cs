2aab8e70b244ab5f923d68904a8362d6923e5817
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-09-05 15:43:26 +0300

    Delete Contact Added to all layer

--- Program.cs
+++ Program.cs
@@ -33,8 +33,8 @@ namespace ContactsConsoleAppPresentationLayer
         {
             clsContact Contact1 = new clsContact();
 
-            Contact1.FirstName = "Bilal";
-            Contact1.LastName = "Mohammed";
+            Contact1.FirstName = "Bilo";
+            Contact1.LastName = "Nur";
             Contact1.Email = "bilmoh@gmail";
             Contact1.Phone  = "0902220108";
             Contact1.Address = "123 A.A";
@@ -54,32 +54,55 @@ namespace ContactsConsoleAppPresentationLayer
 
         public static void testUpdateContact(int ID)
         {
-            clsContact Contact1 = new clsContact();
+            clsContact Contact1 = clsContact.Find(ID);
 
-            Contact1.ID = ID;
-            Contact1.FirstName = "Jemila";
-            Contact1.LastName = "Abdellah";
-            Contact1.Email = "Jem@gmail";
-            Contact1.Phone = "0904440108";
-            Contact1.Address = "123 Jimma";
-            Contact1.DateOfBirth = new DateTime(2003, 01, 17, 10, 30, 0);
-            Contact1.CountryID = 5;
-            Contact1.ImagePath = "";
-
-            if (Contact1.Save())
+            if (Contact1 != null)
             {
-                Console.WriteLine("Contact Updated Successfully:-)");
+                Contact1.ID = ID;
+                Contact1.FirstName = "Jemila";
+                Contact1.LastName = "Abdellah";
+                Contact1.Email = "Jem@gmail";
+                Contact1.Phone = "0904440108";
+                Contact1.Address = "123 Jimma";
+                Contact1.DateOfBirth = new DateTime(2003, 01, 17, 10, 30, 0);
+                Contact1.CountryID = 5;
+                Contact1.ImagePath = "";
+
+
+                if (Contact1.Save())
+                {
+                    Console.WriteLine("Contact Updated Successfully:-)");
+                }
+                else
+                {
+                    Console.WriteLine("Faild to Update Contact:-(");
+                }
             }
+
             else
             {
                 Console.WriteLine("Faild to Update Contact:-(");
+            }
+
+        }
+
+        static void testDeleteContact(int ID)
+        {
+            if(clsContact.DeleteContact(ID))
+            {
+                Console.WriteLine("Contact Deletede Successfully:-)");
+            }
+            else
+            {
+                Console.WriteLine("Contact Deletetion Failed:-(");
             }
         }
         static void Main(string[] args)
         {
             //testFindContactByID(1);
             //testAddNewContact();
-            testUpdateContact(4);
+            //testUpdateContact(8);
+            testDeleteContact(10);
             Console.ReadKey();
         }
     }
